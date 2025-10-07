f8758ecdb0edd8ff32982fe4ce28c8188c80eddb
devNikhil00 <nikhilprajapatistaic@gmail.com>
2025-10-08 00:10:17 +0530

    utills changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,14 +6,20 @@ const connectDB = require("./db/connection");
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database", error);
+    process.exit(1);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello from CommonJS backend!");
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
---
+++ backend/src/utils/apiError.js
@@ -0,0 +1,25 @@
+class apiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        statck = ""
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.errors = errors;
+        this.data = null;
+        this.message = message;
+        this.success = false;
+
+
+        if(statck){
+            this.stack = statck;
+        }else{
+            Error.captureStackTrace(this, this.constructor);
+        }
+
+    }
+}
+
+module.exports = {apiError};
